6cb7e7358ff421677cc7dc631bd3a0464e51bd63
paco <68790836+PacichaBG@users.noreply.github.com>
2023-03-26 17:24:34 +0300

    ok

---
+++ pm/Assets/movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+    }
+}
