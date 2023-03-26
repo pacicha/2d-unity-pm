15c9a5a15e11fee3236111b4da6c5ac6abec80b2
paco <68790836+PacichaBG@users.noreply.github.com>
2023-03-26 21:41:05 +0300

    ok

---
+++ pm/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform target; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+}
--- pm/Assets/move.cs
+++ pm/Assets/move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    Vector2 moved;
+    Vector2 movee;
     public int speed;
 
     void Start()
@@ -13,16 +13,16 @@ public class move : MonoBehaviour
     }
     void Update()
     {
-        moved = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movee = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        transform.Translate(moved * speed * Time.deltaTime,Space.World);
+        transform.Translate(movee * speed * Time.deltaTime,Space.World);
 
         flip();
     }
     void flip()
     {
-        if (moved.x < -0.01f) transform.localScale = new Vector3(-1, 1, 1);
-        if (moved.x > 0.01f) transform.localScale = new Vector3(1, 1, 1);
+        if (movee.x < -0.01f) transform.localScale = new Vector3(-1, 1, 1);
+        if (movee.x > 0.01f) transform.localScale = new Vector3(1, 1, 1);
 
     }
 }
