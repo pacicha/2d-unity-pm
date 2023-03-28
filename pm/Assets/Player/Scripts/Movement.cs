6192ff060f3dedd28fba24b436eb9307c3530783
paco <68790836+PacichaBG@users.noreply.github.com>
2023-03-28 22:55:00 +0300

    ok

--- pm/Assets/Player/Scripts/Movement.cs
+++ pm/Assets/Player/Scripts/Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jump : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] int jumpPower;
@@ -11,16 +11,29 @@ public class jump : MonoBehaviour
     public LayerMask groundLayer;
     bool isGrounded;
     Vector2 vecGravity;
+    private Animator anim;
+    private float movee;
+    public float speed;
 
     void Start()
     {
         vecGravity = new Vector2(0, -Physics2D.gravity.y);
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    void flip()
+    {
+        if (rb.velocity.x < -0.01f) transform.eulerAngles = new Vector3(0, 180, 0);
+        if (rb.velocity.x > 0.01f) transform.eulerAngles = new Vector3(0, 0, 0);
+    }
     void Update()
     {
+        movee = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * movee, rb.velocity.y);
+
+        flip();
+
         isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.4f, 0.37f), CapsuleDirection2D.Horizontal, 0, groundLayer);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -30,5 +43,7 @@ public class jump : MonoBehaviour
         {
             rb.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
         }
+        anim.SetBool(name:"IsInAir", value:!isGrounded);
+        anim.SetBool(name:"IsWalking", value: movee != 0);
     }
 }
